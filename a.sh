4e1e80ca3c2f094c4e509a0f51aded51a34999fe
ComeBey <46746974+ComeBey@users.noreply.github.com>
2019-02-28 23:23:15 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+
+#=================================================
+#	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
+#	Description: Install the ShadowsocksR mudbjson server
+#	Version: 1.0.26
+#	Author: Toyo
+#	Blog: https://doub.io/ss-jc60/
+#=================================================
+
 sh_ver="1.0.26"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
