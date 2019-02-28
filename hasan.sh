a75c8fd650fe5726e5eb9e3bcfbd24c54466e86b
ComeBey <46746974+ComeBey@users.noreply.github.com>
2019-02-28 23:32:36 +0800

    Update hasan.sh

--- hasan.sh
+++ hasan.sh
@@ -2,13 +2,13 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-#=================================================
+#==============================================================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
 #	Description: Install the ShadowsocksR mudbjson server
-#	Version: 1.0.26
+#	Version: 1.0.26                            
+#	youtube关注https://www.youtube.com/channel/UCiMTLB5vELIq3Dz9muBRfJw?view_as=subscriber
 #	
-#	
-#=================================================
+#==============================================================================================
 
 sh_ver="1.0.26"
 filepath=$(cd "$(dirname "$0")"; pwd)
